b34ca7365a383395a497f1bf21f6e49e090a718a
B_E32200722_Lailatul Khusnia <lailatulkhusnia17@gmail.com>
2021-04-25 22:48:02 +0700

    Tugas Minggu 6

---
+++ Tugas Minggu 6/routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Frontend\HomeController;
+use App\Http\Controllers\Backend\DashboardController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//ini adalah bab minggu ke 5
+route::group(['namespace' => 'Frontend'], function()
+	{
+		route::resource('home', 'HomeController');
+	});
+route::group(['namespace' => 'Backend'], function()
+	{
+		route::resource('dashboard', 'DashboardController');
+	});	
